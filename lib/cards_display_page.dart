a639d8c7287282fcd1f7c3811d1bbc71cd5090fa
Pratiksha.Kulkarni <Jio@12123>
2024-03-28 11:53:55 +0530

    background image added

--- lib/cards_display_page.dart
+++ lib/cards_display_page.dart
@@ -10,25 +10,40 @@ class CardsDisplayPage extends StatefulWidget {
 }
 
 class _CardsDisplayPageState extends State<CardsDisplayPage> {
-  List<int> cards =
-      List.generate(13, (index) => index); // List to hold the cards
+  // List to hold the cards
+  late List<int> cards;
   int? startIndex;
   int? indexAfterDrag;
   Suit suit = Suit.spades;
   CardValue value = CardValue.eight;
 
   @override
+  void initState() {
+    super.initState();
+    initializeCardOrder();
+  }
+
+  void initializeCardOrder() {
+    List<int> cards = List.generate(13, (index) => index);
+    cards.shuffle();
+  }
+
+  @override
   Widget build(BuildContext context) {
     var rightPadding = MediaQuery.of(context).size.width * 0.10;
-    var leftPadding = MediaQuery.of(context).size.width * 0.10;
+    var leftPadding = MediaQuery.of(context).size.width * 0.30;
     return Container(
       decoration: BoxDecoration(
+          image: const DecorationImage(
+            image: AssetImage('assets/backgound.jfif'),
+            fit: BoxFit.fitWidth,
+          ),
           gradient: LinearGradient(
-        colors: [
-          const Color(0xFF025043).withOpacity(0.5),
-          const Color(0xFF235347),
-        ],
-      )),
+            colors: [
+              const Color(0xFF025043).withOpacity(0.5),
+              const Color(0xFF235347),
+            ],
+          )),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: LayoutBuilder(builder: (context, constraints) {
@@ -44,40 +59,37 @@ class _CardsDisplayPageState extends State<CardsDisplayPage> {
                   child: Padding(
                     padding: EdgeInsets.only(
                         right: rightPadding, left: leftPadding, bottom: 20),
-                    child: Center(
-                      child: DragTarget<int>(
-                        onAccept: (int draggedIndex) {
-                          setState(() {
-                            // Reorder the cards when dropped
-                            cards.remove(draggedIndex);
-                            cards.insert(index, draggedIndex);
-                          });
-                        },
-                        builder: (BuildContext context,
-                            List<int?> candidateData,
-                            List<dynamic> rejectedData) {
-                          return Draggable(
-                            childWhenDragging: CardsWidget(
-                              index: cards[index],
-                            ),
-                            onDragEnd: (v) {
-                              print("DDD ${v.offset}");
-                            },
-                            data: cards[index], // Use the card value as data
-                            feedback: CardsWidget(index: cards[index]),
-                            onDragStarted: () {
-                              setState(() {
-                                startIndex = index;
-                              });
-                            },
-                            onDragCompleted: () {
-                              assert(startIndex != null);
-                              print("");
-                            },
-                            child: CardsWidget(index: cards[index]),
-                          );
-                        },
-                      ),
+                    child: DragTarget<int>(
+                      onAccept: (int draggedIndex) {
+                        setState(() {
+                          // Reorder the cards when dropped
+                          cards.remove(draggedIndex);
+                          cards.insert(index, draggedIndex);
+                        });
+                      },
+                      builder: (BuildContext context, List<int?> candidateData,
+                          List<dynamic> rejectedData) {
+                        return Draggable(
+                          childWhenDragging: CardsWidget(
+                            index: cards[index],
+                          ),
+                          onDragEnd: (v) {
+                            print("DDD ${v.offset}");
+                          },
+                          data: cards[index], // Use the card value as data
+                          feedback: CardsWidget(index: cards[index]),
+                          onDragStarted: () {
+                            setState(() {
+                              startIndex = index;
+                            });
+                          },
+                          onDragCompleted: () {
+                            assert(startIndex != null);
+                            print("");
+                          },
+                          child: CardsWidget(index: cards[index]),
+                        );
+                      },
                     ),
                   ),
                 );
